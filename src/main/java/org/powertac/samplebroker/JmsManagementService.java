a6e19646b02bde7820d52a72eab29d1704fefc79
Nguyen Nguyen <nguyen@entagen.com>
2012-02-03 01:54:04 -0600

    Configurable JMS broker URL (properties file and optional commandline arg)

--- src/main/java/org/powertac/samplebroker/JmsManagementService.java
+++ src/main/java/org/powertac/samplebroker/JmsManagementService.java
@@ -20,14 +20,13 @@ import java.util.Map;
 import java.util.concurrent.Executor;
 
 import javax.annotation.Resource;
-import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
 import javax.jms.MessageListener;
-import javax.jms.Queue;
-import javax.jms.Session;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.pool.PooledConnectionFactory;
 import org.apache.log4j.Logger;
+import org.powertac.common.config.ConfigurableValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.listener.AbstractMessageListenerContainer;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
@@ -46,36 +45,16 @@ public class JmsManagementService {
   
   @Autowired
   private Executor taskExecutor;
-
-  // JMS Queue Names
-  private String serverQueueName = "serverInput";
+  
+  @Autowired
+  private BrokerPropertiesService brokerPropertiesService;
+  
+  // configurable parameters
+  private String serverQueueName = "serverInput"; 
+  private String jmsBrokerUrl = "tcp://localhost:61616";  
 
   private Map<MessageListener,AbstractMessageListenerContainer> listenerContainerMap = 
       new HashMap<MessageListener,AbstractMessageListenerContainer>();
-  
-  public String getServerQueueName()
-  {
-    return serverQueueName;
-  }
-  
-  public void initializeQueues(String brokerQueueName) {
-    // create server and broker queues
-    createQueue(serverQueueName);
-    createQueue(brokerQueueName);
-  }
-  
-  public Queue createQueue(String queueName) {
-    Queue queue = null;
-    log.info("Creating queue " + queueName);
-    try {
-      Connection connection = connectionFactory.createConnection();
-      Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-      queue = session.createQueue(queueName);
-    } catch (JMSException e) {
-      log.error("Failed to create queue " + queueName, e);
-    }
-    return queue;
-  }
   
   public void registerMessageListener(String destinationName, MessageListener listener) {    
     log.info("registerMessageListener(" + destinationName + ", " + listener + ")");
@@ -89,4 +68,50 @@ public class JmsManagementService {
     
     listenerContainerMap.put(listener, container);
   }
+  
+  public void init (String overridenBrokerUrl)
+  {
+    brokerPropertiesService.configureMe(this);
+    if (overridenBrokerUrl != null && !overridenBrokerUrl.isEmpty()) {
+      setJmsBrokerUrl(overridenBrokerUrl);
+    }
+    
+    if (connectionFactory instanceof PooledConnectionFactory) {
+      PooledConnectionFactory pooledConnectionFactory = (PooledConnectionFactory) connectionFactory;
+      if (pooledConnectionFactory.getConnectionFactory() instanceof ActiveMQConnectionFactory) {
+        ActiveMQConnectionFactory amqConnectionFactory = (ActiveMQConnectionFactory) pooledConnectionFactory
+                .getConnectionFactory();
+        amqConnectionFactory.setBrokerURL(getJmsBrokerUrl());
+      }
+    }
+  }
+  
+  public String getServerQueueName()
+  {
+    return serverQueueName;
+  }
+  /**
+   * @param serverQueueName the serverQueueName to set
+   */
+  public void setServerQueueName (String serverQueueName)
+  {
+    this.serverQueueName = serverQueueName;
+  }  
+  /**
+   * @return the jmsBrokerUrl
+   */
+  public String getJmsBrokerUrl ()
+  {
+    return jmsBrokerUrl;
+  }
+
+  /**
+   * @param jmsBrokerUrl the jmsBrokerUrl to set
+   */
+  @ConfigurableValue(valueType = "String",
+          description = "JMS broker URL to use")  
+  public void setJmsBrokerUrl (String jmsBrokerUrl)
+  {
+    this.jmsBrokerUrl = jmsBrokerUrl;
+  }  
 }
