c52f5ddaca10270504c59c5c13283f620b268b87
Erik Onarheim <onarh001@umn.edu>
2012-02-20 21:17:04 -0600

    Parsing not quite right

--- src/main/java/org/powertac/samplebroker/BrokerMain.java
+++ src/main/java/org/powertac/samplebroker/BrokerMain.java
@@ -16,68 +16,84 @@
 package org.powertac.samplebroker;
 
 //import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import joptsimple.*;
 
 /**
  * This is the top level of the Power TAC server.
+ * 
  * @author John Collins
  */
-public class BrokerMain
-{
-  //static private Logger log = Logger.getLogger(BrokerMain.class);
+public class BrokerMain {
+	// static private Logger log = Logger.getLogger(BrokerMain.class);
 
-  /**
-   * Sets up the broker. Single command-line arg is the username
-   */
-  public static void main (String[] args)
-  {
-    AbstractApplicationContext context = new ClassPathXmlApplicationContext("broker.xml");
-    context.registerShutdownHook();
-    
-    // Get username from command-line.
-    String username = "Sample";
-    String jmsBrokerUrl = null;    
-    String tsUrl = null;
-    String authToken = null;
-    String gameToken = null;
-    
-    
-    if (args.length < 1) {
-      System.out.println("Username not given - default is 'Sample'");
-    }
-    else {
-      username = args[0];
-      if (args.length == 2) {
-        jmsBrokerUrl = args[1];
-      }else if(args.length == 3) {
-    	if(args[1].equalsIgnoreCase("-t")){
-    		tsUrl = args[2];
-    	}
-      }
-      
-    }
-    
-    // If a tournament scheduler was specified try to connect
-    if(tsUrl!=null){
-    	
-    }
-    
-    // find the Broker and JmsManagementService beans, hook up the jms queue
-    SampleBroker broker =
-        (SampleBroker)context.getBeansOfType(SampleBroker.class).values().toArray()[0];
-    broker.init(username);
-    JmsManagementService jmsm =
-        (JmsManagementService)context.getBeansOfType(JmsManagementService.class).values().toArray()[0];
-    BrokerMessageReceiver receiver =
-        (BrokerMessageReceiver)context.getBeansOfType(BrokerMessageReceiver.class).values().toArray()[0];
-    String brokerQueueName = broker.getBroker().toQueueName();
-    
-    jmsm.init(jmsBrokerUrl);
-    jmsm.registerMessageListener(brokerQueueName, receiver);
-    broker.run();
-    
-    // if we get here, it's time to exit
-    System.exit(0);
-  }
+	/**
+	 * Sets up the broker. Single command-line arg is the username
+	 */
+	public static void main(String[] args) {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"broker.xml");
+		context.registerShutdownHook();
+
+		BrokerTournamentService brokerTournamentService = (BrokerTournamentService) context
+				.getBeansOfType(BrokerTournamentService.class).values()
+				.toArray()[0];
+		
+
+		// Get username from command-line.
+		String username = "Sample";
+		String jmsBrokerUrl = null;
+		String tsUrl = null;
+		String authToken = null;
+		String gameToken = null;
+		OptionParser parser = new OptionParser("u:j:t::");
+		OptionSet options = parser.parse(args);
+
+		/*
+		 * if (args.length < 1) {
+		 * System.out.println("Username not given - default is 'Sample'"); }
+		 * else { username = args[0]; if (args.length == 2) { jmsBrokerUrl =
+		 * args[1]; }else if(args.length == 3) {
+		 * if(args[1].equalsIgnoreCase("-t")){ tsUrl = args[2]; // if a tsUrl is
+		 * specified override the config, else use the config } }
+		 * 
+		 * }
+		 */
+
+		if (options.has("t") ^ (options.has("u") && options.has("j"))) {
+			tsUrl = (String) options.valueOf("t");
+			// If a tournament scheduler was specified try to connect to it
+			if (tsUrl != null) {
+				brokerTournamentService.init();
+				jmsBrokerUrl = brokerTournamentService.login(tsUrl);
+			} else {
+				username = (String) options.valueOf("u");
+				jmsBrokerUrl = (String) options.valueOf("j");
+			}
+		} else {
+			System.out.println("Has -t option: " + options.has("t"));
+			System.out
+					.println("Usage: {-u username -j jmsUrl } | {-t tournamentSchedulerUrl }");
+			System.exit(0);
+		}
+
+		// find the Broker and JmsManagementService beans, hook up the jms queue
+		SampleBroker broker = (SampleBroker) context
+				.getBeansOfType(SampleBroker.class).values().toArray()[0];
+		broker.init(username);
+		JmsManagementService jmsm = (JmsManagementService) context
+				.getBeansOfType(JmsManagementService.class).values().toArray()[0];
+		BrokerMessageReceiver receiver = (BrokerMessageReceiver) context
+				.getBeansOfType(BrokerMessageReceiver.class).values().toArray()[0];
+		String brokerQueueName = broker.getBroker().toQueueName();
+
+		jmsm.init(jmsBrokerUrl);
+		jmsm.registerMessageListener(brokerQueueName, receiver);
+		broker.run();
+
+		// if we get here, it's time to exit
+		System.exit(0);
+	}
 }
--- src/main/java/org/powertac/samplebroker/BrokerTournamentService.java
+++ src/main/java/org/powertac/samplebroker/BrokerTournamentService.java
@@ -1,28 +1,61 @@
 package org.powertac.samplebroker;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Properties;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.apache.log4j.Logger;
+import org.powertac.common.config.ConfigurableValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BrokerTournamentService {
+public class BrokerTournamentService{
+
+	static private Logger log = Logger.getLogger(BrokerMessageReceiver.class);
+	
+	@Autowired
+	private BrokerPropertiesService brokerPropertiesService;
 
 	// The game specific token is only valid for one game
 	private String gameToken = null;
+	private String jmsUrl = null;
 
 	// Configurable parameters
 	private String authToken = null;
 	private String tourneyName = null;
 	private String responseType = null;
-	private int maxTry = 1;
+	private String username = null;
 
+	// If set to negative number infinite retries
+	private int maxTry = 1;
+	
+	public void init(){
+		brokerPropertiesService.configureMe(this);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	@ConfigurableValue(valueType = "String",
+	          description = "Set the username of broker for tournament")  
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 	public String getResponseType() {
 		return responseType;
 	}
 
+	@ConfigurableValue(valueType = "String",
+	          description = "Response type to receive from the TS xml or json")  
 	public void setResponseType(String responseType) {
 		this.responseType = responseType;
 	}
@@ -31,14 +64,19 @@ public class BrokerTournamentService {
 		return tourneyName;
 	}
 
+	@ConfigurableValue(valueType = "String",
+	          description = "Name of tournament to join")  
 	public void setTourneyName(String tourneyName) {
 		this.tourneyName = tourneyName;
 	}
 
+	
 	public int getMaxTry() {
 		return maxTry;
 	}
 
+	@ConfigurableValue(valueType = "Integer",
+	          description = "Maximum number of tries to connect to Tournament Scheduler")  
 	public void setMaxTry(int maxTry) {
 		this.maxTry = maxTry;
 	}
@@ -47,6 +85,8 @@ public class BrokerTournamentService {
 		return authToken;
 	}
 
+	@ConfigurableValue(valueType = "String",
+	          description = "Broker unique authorization token to authenticate with Tournament Scheduler")  
 	public void setAuthToken(String authToken) {
 		this.authToken = authToken;
 	}
@@ -59,39 +99,89 @@ public class BrokerTournamentService {
 		this.gameToken = gameToken;
 	}
 
+	// Spins current login attemt for n seconds and url to retry
+	private void spin(int seconds) {
+		try {
+
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// unable to sleep
+			e.printStackTrace();
+		}
+	}
+
 	private boolean loginMaybe(String tsUrl) {
 
 		try {
+			// Build proper connection string to tournament scheduler for
+			// login
 			String restAuthToken = "authToken=" + this.authToken + "&";
 			String restTourneyName = "requestJoin=" + this.tourneyName + "&";
 			String restResponseType = "type=" + this.responseType;
-			URL url = new URL(tsUrl + "?" + restAuthToken + restTourneyName
-					+ restResponseType);
+			String finalUrl = tsUrl + "?" + restAuthToken + restTourneyName
+					+ restResponseType;
+			log.info("Connecting to TS with " + finalUrl);
+
+			URL url = new URL(finalUrl);
 			URLConnection conn = url.openConnection();
 
 			// Get the response
-			BufferedReader input = new BufferedReader(new InputStreamReader(
-					conn.getInputStream()));
+			InputStream input = conn.getInputStream();
+
+			if (this.responseType.compareTo("xml") == 0) {
+				Properties xml = new Properties();
+				xml.loadFromXML(input);
+
+				// Three different message tags
+				String checkRetry = xml.getProperty("retry");
+				String checkLogin = xml.getProperty("login");
+				String checkDone = xml.getProperty("done");
+
+				if (checkRetry != null) {
+					log.info("Retry message received for : " + checkRetry
+							+ " seconds");
+					// Received retry message spin and try again
+					spin(Integer.parseInt(checkRetry));
+					return false;
+				} else {
+					log.fatal("Invalid message type recieved");
+					return false;
+
+				}
+
+				// TODO parse login success
+				// TODO parse done success
+			} else { // response type was json parse accordingly
+				// TODO: Json Parsing
+			}
 
 			return true;
-		} catch (Exception e) {
-
+		} catch (Exception e) { // exception hit return false
+			log.fatal("Error making connection to Tournament Scheduler");
+			log.fatal(e.getMessage());
 			return false;
 		}
 
 	}
 
+	// Returns the game token on success, null on failure
 	public String login(String tsUrl) {
 		if (this.authToken != null && tsUrl != null) {
-			if (tsUrl.indexOf("brokerLogin.jsp") > 0) {
-				while (!loginMaybe(tsUrl) && maxTry > 0) {
-					maxTry--;
+			while (maxTry-- > 0) {
+				System.out.println("Connecting...");
+				if (loginMaybe(tsUrl)) {
+					log.info("Login Successful! Game token: "+ this.gameToken);
+					return this.jmsUrl;
 				}
-			} else {
-				return null;
+				System.out.println("Retries left: " + maxTry);
 			}
+			log.fatal("Max attempts to log in reached");
+		}else{
+			log.fatal("Incorrect Tournament Scheduler URL or Broker Auth Token");
 		}
-		return tsUrl;
+		return null;
 	}
+
+
 
 }
